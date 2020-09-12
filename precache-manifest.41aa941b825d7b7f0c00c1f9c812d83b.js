fe385f13ecc154da87f29176cc735ec978ace3d1
vs4711 <vijendersharma47@gmail.com>
2020-09-13 00:27:40 +0530

    Updates

---
+++ precache-manifest.41aa941b825d7b7f0c00c1f9c812d83b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144cee8fc9e320d6b8691d5332bc22a4",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "c88f225ea855adf83652",
+    "url": "/Emojipedia/static/js/2.72bec74e.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/Emojipedia/static/js/2.72bec74e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c7c4a189a733c725af0",
+    "url": "/Emojipedia/static/js/main.69ad0f5c.chunk.js"
+  },
+  {
+    "revision": "ebc739c0b131137c4617",
+    "url": "/Emojipedia/static/js/runtime-main.a18d6eed.js"
+  }
+]);
